8683088eb4ab5d636278462677a0a0b7d8152753
KimTaewan <wwan13@naver.com>
2021-01-16 16:10:12 +0900

    dip

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -8,9 +8,12 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
 
-        // command + option + v
+        /* AppConfig 를 통해 memberservice 인스턴스 생성 -> client 코드에서 직접 건들지 않아도 됨 */
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+
+        /* 멤버 생성 후 테스트 */
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
