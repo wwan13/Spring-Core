e34f6595a8fe083c14c7f79651d97b51adba626f
KimTaewan <wwan13@naver.com>
2021-01-19 17:33:03 +0900

    @Configuration and byte code

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -28,4 +28,12 @@ public class ConfigurationSingletonTest {
 
         Assertions.assertThat(memberRepository1).isSameAs(memberRepository2);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
 }
