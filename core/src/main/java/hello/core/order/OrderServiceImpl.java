18e47ce96863b9d7f5ed69b5e76fa9a711777670
KimTaewan <wwan13@naver.com>
2021-01-19 17:01:34 +0900

    @Configuration and singleton

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -24,4 +24,9 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+
+    // code for test
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
