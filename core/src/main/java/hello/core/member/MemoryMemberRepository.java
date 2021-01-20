6477191e8ce5bd8f6273c5bfdb6ddfc2a4b7e6b8
KimTaewan <wwan13@naver.com>
2021-01-20 20:49:53 +0900

    component scan  and Spring DI

--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,12 +1,14 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
-
 
     @Override
     public void save(Member member) {
